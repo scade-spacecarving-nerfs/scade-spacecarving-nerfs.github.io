adee04e7b85089f37e09e0bb62bfc8147a777f4c
mikacuy <muy@adobe.com>
2023-06-04 04:51:00 -0700

    update

--- js/footable.js
+++ js/footable.js
@@ -7367,3 +7367,62 @@
 	};
 
 })(FooTable);
+
+
+const carousel = document.getElementById("carouselExampleControls");
+const items = carousel.querySelectorAll(".carousel-item");
+let currentItem = 0;
+let isActive = true;
+
+function setCurrentItem(index) {
+  currentItem = (index + items.length) % items.length;
+}
+
+function hideItem(direction) {
+  isActive = false;
+  items[currentItem].classList.add(direction);
+  items[currentItem].addEventListener("animationend", function() {
+    this.classList.remove("active", direction);
+  });
+}
+
+function showItem(direction) {
+  items[currentItem].classList.add("next", direction);
+  items[currentItem].addEventListener("animationend", function() {
+    this.classList.remove("next", direction);
+    this.classList.add("active");
+    isActive = true;
+  });
+}
+
+document.getElementById("carouselPrev").addEventListener("click", function(e) {
+  e.preventDefault();
+  if (isActive) {
+    hideItem("to-right");
+    setCurrentItem(currentItem - 1);
+    showItem("from-left");
+  }
+});
+
+document.getElementById("carouselNext").addEventListener("click", function(e) {
+  e.preventDefault();
+  if (isActive) {
+    hideItem("to-left");
+    setCurrentItem(currentItem + 1);
+    showItem("from-right");
+  }
+});
+
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
